de1470b86a15fe13fc9c164939261283931ff2f1
jaypsondarva <jaysondarva1247@gmail.com>
2026-04-24 00:09:15 +0530

    Create practical13.java

---
+++ practical13.java
@@ -0,0 +1,50 @@
+class Rectangle {
+    double width;
+    double height;
+
+    Rectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    double getArea() {
+        return width * height;
+    }
+
+
+    double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+  
+    void display() {
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+      
+    }
+
+    public static void main(String[] args) {
+
+  
+        Rectangle r1 = new Rectangle(4, 40);
+        Rectangle r2 = new Rectangle(3.5, 35.9);
+        System.out.println("Name : Jay");
+        System.out.println(Enrollment: 240390107051");
+        System.out.println("Rectangle 1:");
+        r1.display();
+
+        System.out.println("Rectangle 2:");
+        r2.display();
+
+       
+        if (r1.getArea() > r2.getArea()) {
+            System.out.println("Rectangle 1 has a larger area.");
+        } else if (r1.getArea() < r2.getArea()) {
+            System.out.println("Rectangle 2 has a larger area.");
+        } else {
+            System.out.println("Both rectangles have equal area.");
+        }
+    }
+}
