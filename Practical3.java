da9c33658ce6623796741facc6ca9b19f71e62e5
jaypsondarva <jaysondarva1247@gmail.com>
2026-04-23 16:58:55 +0530

    Update and rename VowelOrConsonant.java to Practical3.java

--- Practical3.java
+++ Practical3.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class VowelOrConsonant {
+public class practical3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+         System.out.print("Name: Jaykumar Pravinbhai sondarva ");
+         System.out.print("Enrollment No:240390107051 ");
         System.out.print("Enter a single letter: ");
         String input = scanner.nextLine();
         
